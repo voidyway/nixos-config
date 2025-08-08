30d9153e3df29144068d3c7b14f2552d9d8099df
voidyway <175210480+voidyway@users.noreply.github.com>
2025-08-08 06:27:51 +0300

    Fix Fix Colors over SSH

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,6 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     shell = pkgs.fish;
-
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDD02Y9FLoeScsjEyZ/vgFM5ufW84fkJPbvZzUUaR78Q voidy" # macbook
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDC+r5aiSWMp9a6sOg+zQFseTKAVWZWA3cOXWdKtQYIb u0_a124" # tablet's termux
@@ -36,7 +35,7 @@
       enable = true;
       interactiveShellInit = ''
         set fish_greeting # Disable greeting
-        export $COLORTERM="truecolor"
+        export COLORTERM="truecolor" # fix iterm2 ssh colors
       '';
     };
 
@@ -59,8 +58,15 @@
           email = "175210480+voidyway@users.noreply.github.com";
         };
       };
-
     };
+
+    nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+      flake = "/home/omar/nixos-config";
+    };
+
   };
 
   environment.systemPackages = with pkgs; [
@@ -70,7 +76,6 @@
   ];
 
   services = {
-
     openssh = {
       enable = true;
       settings.PasswordAuthentication = false;
